cdfcdd8972d6cd904120160b46a3c472f005d6e7
Wesley Faria <wfaria49@gmail.com>
2024-03-08 13:20:47 -0300

    Entrega projeto m1-barbearia-web

--- script.js
+++ script.js
@@ -1,45 +1,88 @@
 const barbearia = {
     cortes: [
-        { id: 1, tipo: "Militar", valor: 20 },
-        { id: 2, tipo: "Samurai", valor: 35 },
-        { id: 3, tipo: "Pompadour", valor: 20 },
-        { id: 4, tipo: "Moicano", valor: 10 },
-        { id: 5, tipo: "Razor part", valor: 5 },
+      { id: 1, tipo: "Militar", valor: 20 },
+      { id: 2, tipo: "Samurai", valor: 35 },
+      { id: 3, tipo: "Pompadour", valor: 20 },
+      { id: 4, tipo: "Moicano", valor: 10 },
+      { id: 5, tipo: "Razor part", valor: 5 },
     ],
   
     barbas: [
-        { id: 1, tipo: "Capitão Jack", valor: 20 },
-        { id: 2, tipo: "Van Dyke", valor: 20 },
-        { id: 3, tipo: "Barba Média", valor: 20 },
-        { id: 4, tipo: "Barba Baixa", valor: 20 },
-        { id: 5, tipo: "Barba Zero", valor: 15 },
+      { id: 1, tipo: "Capitão Jack", valor: 20 },
+      { id: 2, tipo: "Van Dyke", valor: 20 },
+      { id: 3, tipo: "Barba Média", valor: 20 },
+      { id: 4, tipo: "Barba Baixa", valor: 20 },
+      { id: 5, tipo: "Barba Zero", valor: 15 },
     ],
     estaAberto: true,
-};
+  };
   
+  function buscaCortePorId(id) {
+    for (let i = 0; i < barbearia.cortes.length; i++) {
+      if (barbearia.cortes[i].id === id) {
+        return barbearia.cortes[i];
+      }
+    }
+    return "Corte não encontrado";
+  }
+  buscaCortePorId(1);
   
-function buscaCortePorId(id) {
-}
-
-function buscaBarbaPorId(id) {
-}
-
-function verificaStatusBarbearia() {
-}
-
-function retornaTodosCortes() {
-}
-
-function retornaTodasBarbas() {
-}
-
-function criaPedido(nomeCliente, corteId, barbaId) {
-}
-
-function atualizarServico(lista, id, valor, tipo) {
-}
-
-function calculaTotal(pedido) {
-}
+  function buscaBarbaPorId(id) {
+    for (let i = 0; i < barbearia.barbas.length; i++) {
+      if (barbearia.barbas[i].id === id) {
+        return barbearia.barbas[i];
+      }
+    }
+    return "Barba não encontrada";
+  }
+  buscaBarbaPorId(5);
   
+  function verificaStatusBarbearia() {
+    if (!barbearia.estaAberto) {
+      return "Estamos Fechados";
+    }
+    return "Estamos abertos";
+  }
+  verificaStatusBarbearia();
+  
+  function retornaTodosCortes() {
+    return barbearia.cortes;
+  }
+  retornaTodosCortes();
+  
+  function retornaTodasBarbas() {
+    return barbearia.barbas;
+  }
+  retornaTodasBarbas();
+  
+  function criaPedido(nomeCliente, corteId, barbaId) {
+    const corte = buscaCortePorId(corteId);
+    const barba = buscaBarbaPorId(barbaId);
+    const pedido = {
+      nome: nomeCliente,
+      pedidoCorte: corte.tipo,
+      pedidoCortePreco: corte.valor,
+      pedidoBarba: barba.tipo,
+      pedidoBarbaPreco: barba.valor,
+    };
+    return pedido;
+  }
+  criaPedido("Lucas", 4, 3);
+  
+  function atualizarServico(lista, id, valor, tipo) {
+    for (let i = 0; i < lista.length; i++) {
+      if (lista[i].id == id) {
+        lista[i].valor = valor;
+        lista[i].tipo = tipo;
+        return lista;
+      }
+    }
+  }
+  atualizarServico(barbearia, 5, 55, "Terere");
+  
+  function calculaTotal(pedido) {
+    let total = pedido.pedidoCortePreco + pedido.pedidoBarbaPreco;
+    return total;
+  }
+  calculaTotal();
   
